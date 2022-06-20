7d6c542eb512f8a146e19cf2d4493ee9b8e0d49d
WARDI-Kamal <kha0s@free.fr>
2022-06-20 18:09:19 +0400

    wip : rebuild project structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import supabase from '../utils/supabase.js'
 
+// @refresh reset - à mettre n'importe où ds le fichier pour reset le state du component
+
 export async function getStaticProps() {
-let { data: projects, error } = await supabase
-.from('projects')
-.select('*')
-
-console.log('projects', projects)
-if(error) {
-  throw new Error(error.message)
-}
-
+  let { data: projects, error } = await supabase
+  .from('projects')
+  .select('*')
+  
+  if(error) {
+    throw new Error(error.message)
+  }
   return {
     props: {
       projects,
@@ -19,7 +19,7 @@ if(error) {
   };
 }
 
-export default function Home({projects}: {projects: any}) {
+export default function Index({projects}: {projects: any}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -30,6 +30,7 @@ export default function Home({projects}: {projects: any}) {
 
       <main className={styles.main}>
         <pre>{JSON.stringify(projects, null, 2)}</pre>
+        console.log(projects)
       </main>
 
       <footer className={styles.footer}>
