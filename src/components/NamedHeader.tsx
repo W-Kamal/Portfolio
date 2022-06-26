97f764baf255c2b484fe4f2b30db35d7a9091d24
WARDI-Kamal <kha0s@free.fr>
2022-06-26 17:58:15 +0400

    wip : structure and jsx->tsx

---
+++ src/components/NamedHeader.tsx
@@ -0,0 +1,17 @@
+export {}
+
+
+const NamedHeader = () => {
+  return (
+    <div>
+      <header className="App-header">
+      <picture>
+        <img></img>
+      </picture>
+      <span>Wardi Kamal</span>
+      </header>
+    </div>
+  )
+}
+
+export default NamedHeader
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+export {}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import '../styles/global.css'
 
+// ReactDOM.createRoot(document.getElementById('root')!).render(
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
